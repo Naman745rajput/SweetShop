8d7d5c3dd954c52998ee79f5f1cb332e8fee8fdb
Naman745rajput <rajputnaman745@gmail.com>
2025-12-13 13:52:46 +0530

    Implement user registration with passing integration test

---
+++ src/main/java/com/naman/SweetShop/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.naman.SweetShop.controller;
+
+import com.naman.SweetShop.model.User;
+import com.naman.SweetShop.repo.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        if(userRepository.findByUsername(user.getUsername()).isPresent()){
+            return ResponseEntity.badRequest().body("Username already exists");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("user registered successfully");
+    }
+}
--- src/test/java/com/naman/SweetShop/SweetShopApplicationTests.java
+++ src/test/java/com/naman/SweetShop/SweetShopApplicationTests.java
@@ -1,13 +1,38 @@
 package com.naman.SweetShop;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 class SweetShopApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void shouldRegisterNewUser() throws Exception {
+		String newUserJson = """
+				{
+					"username": "candyman",
+					"password": "password123",
+					"role": "USER"
+				}
+			""";
+		mockMvc.perform(post("/api/auth/register")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(newUserJson))
+				.andExpect(status().isCreated());
+
 	}
 
 }
